1ba541da74e31dcf3eddc5e3b2c8b3c159c12a81
Cameron <cameron.oberlies@outlook.com>
2026-03-19 18:57:28 -0500

    add more permanent supabase proxy for flight api

---
+++ supabase/functions/flight-proxy/index.ts
@@ -0,0 +1,27 @@
+import { serve } from 'https://deno.land/std@0.168.0/http/server.ts'
+
+const FLIGHT_API_URL = 'http://45.33.120.41:5000'
+
+serve(async (req) => {
+  // Get the path from the request
+  const url = new URL(req.url)
+  const path = url.pathname.replace('/flight-proxy', '')
+  
+  try {
+    // Proxy the request to your Linode
+    const response = await fetch(`${FLIGHT_API_URL}${path}`)
+    const data = await response.json()
+    
+    return new Response(JSON.stringify(data), {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+    })
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' },
+    })
+  }
+})
